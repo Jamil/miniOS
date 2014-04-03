44b3a6e7ed0a602e4e26d1b81310915531de1944
nicosuarez94 <nicosuarez94@gmail.com>
2014-04-03 16:39:46 -0400

    Update parser.c
    change the parser

--- parser.c
+++ parser.c
@@ -1,6 +1,30 @@
 void newLine(char*);
+char* split_line(char* command);
+int str_cp(char* str1,char* str2);
+void pwd_function(char* arg);
+
+
+char* split_line(char* command){
+
+  //declaration
+  
+  char* temp;
+  temp=command;
+  //whilellop
+  while((*temp)!=0&&(*temp)!=' '){
+    temp++;
+  }
+  //end of while loop
+  if((*temp)==0)
+    return 0;
+  *temp='\0';
+  return temp+1;
+   
+}
+
 
 void parser(char* command){
+  int a=1;
 
   if(*command=='\0'){
     newLine(">>");
@@ -8,7 +32,7 @@ void parser(char* command){
   }
   
   char* args;
-  split_line(command,&args);
+  args=split_line(command);
   
   if(str_cp(command,"pwd")==1)
     pwd_function(args);
@@ -18,19 +42,7 @@ void parser(char* command){
 }
 
 
-void split_line(char* command,char** args_pt){
 
-  char* temp;
-  temp=command;
-  while((*temp)!='\0'||(*temp)!=' '){
-    temp++;
-  }
-  if((*temp)=='\0')
-    *args_pt='\0';
-  *temp='\0';
-  *args_pt=temp+1;
-
-}
 
 int str_cp(char* str1,char* str2){
   int count;
@@ -51,7 +63,7 @@ void pwd_function(char* arg){
   char* pwd_current;
  
  
-  char temp_response="you are home";
+  char *temp_response="you are home";
   pwd_current=temp_response;
   
   
@@ -59,3 +71,6 @@ void pwd_function(char* arg){
   return;
   
 }
+
+
+
