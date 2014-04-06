40c812d69884646c85b419692b9b3278eec7bbd5
nicosuarez94 <nicosuarez94@gmail.com>
2014-04-06 16:59:26 -0400

    Update parser.c
    added new features

--- parser.c
+++ parser.c
@@ -1,26 +1,15 @@
+void parser(char* command);
 void newLine(char*);
 char* split_line(char* command);
 int str_cp(char* str1,char* str2);
 void pwd_function(char* arg);
+void concatinate(char* str1,char* str2);
+void ls_function(char* arg);
+void cd_function(char* arg);
+void execute_function(char* arg);
 
 
-char* split_line(char* command){
-
-  //declaration
-  
-  char* temp;
-  temp=command;
-  //whilellop
-  while((*temp)!=0&&(*temp)!=' '){
-    temp++;
-  }
-  //end of while loop
-  if((*temp)==0)
-    return 0;
-  *temp='\0';
-  return temp+1;
-   
-}
+int foobar asm("newLine") = 0x02000;
 
 
 void parser(char* command){
@@ -36,41 +25,18 @@ void parser(char* command){
   
   if(str_cp(command,"pwd")==1)
     pwd_function(args);
+	
+  if(str_cp(command,"execute")==1)
+    execute_function(args);
+	
+ if(str_cp(command,"ls")==1)
+    ls_function(args);
+	
+  if(str_cp(command,"cd")==1)
+   cd_function(args);
 
   newLine(">>");
   return;
 }
-
-
-
-
-int str_cp(char* str1,char* str2){
-  int count;
-  
-  while(str1[count]==str2[count]&&str1[count]!='\0'&&str2[count]!='\0'){
-      count++;
-  
-  }
-  if(str1[count]!=str2[count])
-   return 0;
-  return 1;
-
-}
-
-
-void pwd_function(char* arg){
-
-  char* pwd_current;
- 
- 
-  char *temp_response="you are home";
-  pwd_current=temp_response;
-  
-  
-  newLine(pwd_current);
-  return;
-  
-}
-
 
 
