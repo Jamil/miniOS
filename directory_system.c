95be9657dbc2d9240c447bc095b234ca9b1fdf0c
nicosuarez94 <nicosuarez94@gmail.com>
2014-04-07 16:48:10 -0400

    added directory system

---
+++ directory_system.c
@@ -0,0 +1,16 @@
+struct file_directory{
+    _file_name file_name;
+	_file_extension file_extension;
+	_file_attributes file_attributes;
+	_reserved reserved;
+	_creation_date;
+	_las_update;
+	uint2_t starting_cluster;
+	
+
+
+
+
+
+
+}
