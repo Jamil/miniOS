8d0fccf2a63af276860506119f04902173531047
nicosuarez94 <nicosuarez94@gmail.com>
2014-04-07 19:45:12 -0400

    update parser.c (the in memory version)

--- parser.c
+++ parser.c
@@ -9,7 +9,7 @@ void cd_function(char* arg);
 void execute_function(char* arg);
 
 
-int newLine asm("newLine") = 0x02000;
+//int foobar asm("newLine") = 0x02000;
 
 
 void parser(char* command){
@@ -39,4 +39,144 @@ void parser(char* command){
   return;
 }
 
+
+
+
+
+char* split_line(char* command){
+
+
+
+  //declaration
+  char* temp;
+  temp=command;
+  //whilellop
+  while((*temp)!=0&&(*temp)!=' '){
+    temp++;
+  }
+  //end of while loop
+  if((*temp)==0)
+    return 0;
+  *temp='\0';
+  return temp+1;
+   
+}
+
+
+
+
+
+int str_cp(char* str1,char* str2){
+  int count=0;
+  
+  while(str1[count]==str2[count]&&str1[count]!='\0'&&str2[count]!='\0'){
+      count++;
+  
+  }
+  if(str1[count]!=str2[count])
+   return 0;
+  return 1;
+
+}
+
+void concatinate(char* str1,char* str2){
+
+  //declaration
+  
+
+  //whilellop
+  char *temp=str1;
+  
+  while((*temp)!='\0'){
+    temp++;
+  }
+  while((*str2)!='\0'){
+    *temp=*str2;
+	temp++;
+	str2++;
+  }
+  *temp='\0';
+  //end of while loop
+  return;
+   
+}
+
+void pwd_function(char* arg){
+
+  char* pwd_current;
+ 
+ 
+  char *temp_response="you are home";
+  pwd_current=temp_response;
+  
+  
+  newLine(pwd_current);
+  return;
+  
+}
+
+
+void execute_function(char* arg){
+
+
+  char* flags=split_line(arg);
+
+  
+
+  char* pwd_current;
+  
+  char *temp_response[100];
+
+  *temp_response='\0';
+  
+  concatinate(temp_response,"executing program ");
+  concatinate(temp_response,arg);
+  
+  
+  
+  pwd_current=temp_response;
+  
+  
+  newLine(pwd_current);
+  
+
+  return;
+  
+}
+
+void ls_function(char* arg){
+
+  char* pwd_current;
+ 
+ 
+  char *temp_response="There is nothing here";
+  pwd_current=temp_response;
+  
+  
+  newLine(pwd_current);
+  return;
+  
+}
+
+
+void cd_function(char* arg){
+
+  char* pwd_current;
+ 
+  char* flags=split_line(arg);
+ 
+  char *temp_response[100];
+
+  *temp_response='\0';
+  
+  concatinate(temp_response,"goin to: ");
+  concatinate(temp_response,arg);
+  
+  pwd_current=temp_response;
+  
+  
+  newLine(pwd_current);
+  return;
+  
+}
 
