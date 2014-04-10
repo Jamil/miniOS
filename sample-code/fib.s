33f93ab6e1bba28bfe8c1f529a1915a6808872d7
Jamil Dhanani <jamil.dhanani@gmail.com>
2014-04-10 17:59:50 -0400

    Updated sample code

--- sample-code/fib.s
+++ sample-code/fib.s
@@ -1,10 +1,30 @@
 .org 0x00130000
-.equ newLine, ???
+.equ newLine, 0x170c
+
+.global main
 
 main:
+  
+  subi sp, sp, 4
+  stw ra, (sp)
+  
+  movia r20, str
+  movi r21, 0x31    # ensure string is 1 to start
+  stb r21, (r20)
+  
+  movia r4, str
+  call newLine
+  movia r4, str
+  call newLine
+  
   movi r4, 5
   call fib
-  br main
+  
+  ldw ra, (sp)
+  addi sp, sp, 4
+  
+done_main:
+  ret
 
 fib:
   movi r16, 1   # fib[k-2]
@@ -12,21 +32,27 @@ fib:
   mov r19, r4
 fib_loop:
   bgt r17, r19, done
-  addi r18, r16, r17    # fib[k]
+  add r18, r16, r17     # fib[k]
   
-  movi r20, str
-  addi r21, r18, 30     # ascii value of number
+  movia r20, str
+  addi r21, r18, 0x30   # ascii value of number
   stb r21, (r20)        # store in first char of str
+  
+  subi sp, sp, 4
+  stw ra, (sp)
   
   mov r4, r20
   call newLine
+  
+  ldw ra, (sp)
+  addi sp, sp, 4
 
-  addi r16, r16, 1
-  addi r17, r17, 1
+  mov r16, r17
+  mov r17, r18
   
   br fib_loop
 done:
-  br done
+  ret
   
- str: 
+str: 
   .asciz "1"
